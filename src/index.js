a67ea22b4793ac259731a9f338a2c4e1f1fc8661
hoanghuy08041977@gmai.com <hoanghuy08041977@gmail.com>
2024-05-08 18:35:50 +0700

    Update

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+function App() {
+  const fullName = "Hoang Duc Huy";
+  const className = "SE17D08";
+  const schoolName = "FBTU";
+  const description = "This is a React application displaying user information. Let's go";
+
+  return (
+    <div>
+      <h1>User Information</h1>
+      <p>Full Name: {fullName}</p>
+      <p>Class: {className}</p>
+      <p>School: {schoolName}</p>
+      <p>Description: {description}</p>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,7 +27,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
