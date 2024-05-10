2be4c17c781812f35d92c2bd95a9d341cf71d385
hoanghuy08041977@gmai.com <hoanghuy08041977@gmail.com>
2024-05-10 13:51:03 +0700

    Example for Arrow function

--- src/index.js
+++ src/index.js
@@ -3,19 +3,46 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-function App() {
-  const fullName = "Hoang Duc Huy";
-  const className = "SE17D08";
-  const schoolName = "FBTU";
-  const description = "This is a React application displaying user information. Let's go";
+let greet = (name, timeOfDay) => {
+  console.log(`Good ${timeOfDay}, ${name}`);
+};
 
+greet("John", "morning");
+greet("Alice", "afternoon");
+
+let square = (num) => {
+  return num * num;
+};
+
+console.log(square(5));
+console.log(square(8));
+
+let person = {
+  name: "John",
+  age: 30,
+  greet: function () {
+    console.log(
+      `Hello, my name is ${this.name} and I'm ${this.age} years old.`
+    );
+  },
+};
+
+person.greet();
+
+function sum(...numbers) {
+  let total = 0;
+  for (let number of numbers) {
+    total += number;
+  }
+  return total;
+}
+
+console.log(`Sum: ${sum(1, 2, 3, 4, 5, 6, 7, 8, 9)}`);
+
+function App() {
   return (
     <div>
-      <h1>User Information</h1>
-      <p>Full Name: {fullName}</p>
-      <p>Class: {className}</p>
-      <p>School: {schoolName}</p>
-      <p>Description: {description}</p>
+      <h1>Let F12</h1>
     </div>
   );
 }
